d59d250488ddb8615456280011fd5d2d55a28675
zhengm <390241476@qq.com>
2017-01-27 01:39:54 +0800

    spirng commit pom

--- schema/sampledb.sql
+++ schema/sampledb.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS sampledb;
 CREATE DATABASE sampledb DEFAULT CHARACTER SET utf8;
 USE sampledb;
 
+##创建用户表
 CREATE TABLE t_user (
    user_id   INT AUTO_INCREMENT PRIMARY KEY,
    user_name VARCHAR(30),
@@ -11,6 +12,7 @@ CREATE TABLE t_user (
    last_ip  VARCHAR(23)
 )ENGINE=InnoDB; 
 
+##创建用户登录日志表
 CREATE TABLE t_login_log (
    login_log_id  INT AUTO_INCREMENT PRIMARY KEY,
    user_id   INT,
@@ -18,6 +20,7 @@ CREATE TABLE t_login_log (
    login_datetime datetime
 )ENGINE=InnoDB; 
 
-INSERT INTO t_user (user_name,password)
+##插入初始化数据
+INSERT INTO t_user (user_name,password) 
              VALUES('admin','123456');
 COMMIT;
