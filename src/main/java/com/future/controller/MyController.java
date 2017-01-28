b545bf5c01ba44b9b044f63d9d21631c221886a9
zhengm <390241476@qq.com>
2017-01-28 09:33:00 +0800

    commit msg

--- src/main/java/com/future/controller/MyController.java
+++ src/main/java/com/future/controller/MyController.java
@@ -1,7 +1,15 @@
 package com.future.controller;
 
+import com.future.domain.User;
+import com.future.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * Created by zhengming on 17/1/27.
@@ -9,9 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
+//    private static Logger LOGGER = Logger.getLogger(MyController.class);
+    @Autowired
+    private UserService userService;
+
     @RequestMapping({"/hello", "lj", "/"})
-    public String hello() {
+    public ModelAndView hello(HttpServletRequest request) {
+//        LOGGER.info("spring mvc begin");
         System.out.println("hello");
-        return "hello";
+        User user = new User();
+        user.setLastIp(request.getLocalAddr());
+        user.setLastVisit(new Date());
+        user.setUserName(request.getParameter("u"));
+        user.setCredits(10);
+//        userService.loginSuccess(user);
+        request.getSession().setAttribute("user", user);
+        return new ModelAndView("main");
     }
 }
